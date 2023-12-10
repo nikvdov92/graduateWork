be1f3c30c9ae2757598fe0e69b70f1c18abaa0d4
Homework3 <tradervdovin@gmail.com>
2023-12-10 14:42:21 +0500

    Web config update

--- src/main/java/com/example/graduatework/config/WebSecurityConfig.java
+++ src/main/java/com/example/graduatework/config/WebSecurityConfig.java
@@ -2,8 +2,9 @@ package com.example.graduatework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,7 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@EnableMethodSecurity
 
 public class WebSecurityConfig {
 
